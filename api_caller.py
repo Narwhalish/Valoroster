42cd1cbe465f5efc1302201bc580226750334f9c
Bryan Yao <bryao@seas.upenn.edu>
2022-04-01 18:04:45 -0400

    Create APICaller class to connect with LiquipediaAPI.

---
+++ api_caller.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+class APICaller:
+    BASE_URL = "https://liquipedia.net/valorant/api.php"
+    BASE_PARAMS = {"action": "parse", "format": "json"}
+
+    def __init__(self):
+        self.response = requests.get(self.BASE_URL, params=self.BASE_PARAMS)
+
+        if self.response.status_code != 200:
+            raise Exception("API call failed")
+
+    def get_page(self, page_name):
+        self.response = requests.get(
+            self.BASE_URL, params=self.BASE_PARAMS | {"page": page_name}
+        )
+
+    def print_response(self):
+        print(self.response.json())
---
+++ main.py
@@ -0,0 +1,7 @@
+import api_caller
+import json
+
+if __name__ == "__main__":
+    foo = api_caller.APICaller()
+
+    foo.print_response()

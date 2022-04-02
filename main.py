e5c652cfed1fa71e6210824d84bb5a5ff19fab76
Bryan Yao <bryao@seas.upenn.edu>
2022-04-02 15:10:12 -0400

    Modify main.py to use cached data instead of calling the API.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-import json
-
 from api_caller import APICaller
-from data_crawler import Scraper
+from tourny_scraper import Scraper
 
 PAGE_NAME = "Fight_Club"
 
 if __name__ == "__main__":
-    caller = APICaller()
+    # caller = APICaller()
 
-    caller.get_page(PAGE_NAME)
-    body = caller.get_json()["parse"]["text"]["*"]
+    # caller.get_page(PAGE_NAME)
+    # body = caller.response.json()["parse"]["text"]["*"]
+
+    # with open("temp2.json", "w") as f:
+    #     f.write(caller.response)
+
+    with open("test_io/test_input.html", "r") as f:
+        body = f.read()
 
     scraper = Scraper(body)
-
-    with open("temp.html", "w") as f:
-        for matchup in scraper.get_matchups():
-            f.write(str(matchup) + "\n")
+    with open("test_io/test_output.txt", "w") as f:
+        for game in scraper.games:
+            f.write(str(game) + "\n")

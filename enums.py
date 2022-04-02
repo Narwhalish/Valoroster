fbbe4160ceaa4a454e39c36ac3aca0795b3dc118
Bryan Yao <bryao@seas.upenn.edu>
2022-04-02 15:12:52 -0400

    Create enum classes Agent and Map.

---
+++ enums.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class Map(Enum):
+    SPLIT = 1
+    HAVEN = 2
+    BIND = 3
+    ASCENT = 4
+    ICEBOX = 5
+    BREEZE = 6
+    FRACTURE = 7
+    DEFAULT = 0
+
+
+class Agent(Enum):
+    BRIMSTONE = 1
+    VIPER = 2
+    OMEN = 3
+    KILLJOY = 4
+    CYPHER = 5
+    SOVA = 6
+    SAGE = 7
+    PHEONIX = 9
+    JETT = 10
+    REYNA = 11
+    RAZE = 12
+    BREACH = 13
+    SKYE = 14
+    YORU = 15
+    ASTRA = 16
+    KAYO = 17
+    CHAMBER = 18
+    NEON = 19
+    DEFAULT = 0
